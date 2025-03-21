8758d6d0f30c7b2678177f3da7226fafd51fc7c5
richard <richard.wasonga@student.moringaschool.com>
2025-03-21 10:38:04 +0300

    complete

--- js/index.js
+++ js/index.js
@@ -19,24 +19,139 @@
 //     console.log(err)
 // })
 
+let base_url = "http://localhost:3000/products"
+
+
 function displayProduct(products){
 
     let product_catalog = document.getElementById('product-catalog')
-    let html = `
+
+    products.forEach(product => {
+        let html = `
         <div class="single-product">
-            <h5>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h5>
-            <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="">
-            <p>our perfect pack for everyday use and walks in th…to 15 inches) in the padded sleeve, your everyday</p>
-            <div>men's clothing</div>
-            <div>$109.95</div>
+            <h5>${product.title.substring(0, 10)}...</h5>
+            <img src="${product.image}" alt="${product.title}">
+            <p>${product.description.substring(0, 30)}...</p>
+            <div>${product.category}</div>
+            <div>$${product.price}</div>
+            <div id="buttons">
+            <button class="one update">Update</button>
+            <button id="delete-btn" class="one delete" onclick="deleteProduct(${product.id})">Delete</button>
+            </div>
         </div>
     `
+        product_catalog.innerHTML += html
+
+        // let btn = document.getElementById('delete-btn')
+        
+        // btn.addEventListener('click', () => deleteProduct(product.id))
+    })
+    
+}
+
+function deleteProduct(product_id){
+
+    console.log("------------------------------")
+    console.log(product_id)
+    fetch(`${base_url}/${product_id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+
+}
+
+function updateProduct(product){
+
 }
 
 
-fetch("https://fakestoreapi.com/products/")
+    let form = document.getElementById('form')
+    form.addEventListener('submit', handleSubmit)
+function handleSubmit(event){
+    event.preventDefault()
+        let product_title = document.getElementById('title').value
+        let product_image = document.getElementById('image').value
+        let product_price = document.getElementById('price').value
+        let product_description = document.getElementById('description').value
+        let product_category = document.getElementById('category').value
+
+        let product_object = {
+            title: product_title,
+            image: product_image,
+            price: product_price,
+            description: product_description,
+            category: product_category
+        }
+
+        postProduct(product_object)
+}
+
+    function postProduct(product_object){
+        fetch(base_url,{
+            method: "POST",
+            headers : {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(product_object)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }
+    
+
+
+
+
+
+// GET method
+
+fetch(base_url)
+.then(res => res.json())
+.then(data => displayProduct(data))
+.catch(err => console.log(err))
+
+// POST method
+
+fetch(base_url, {
+    method: "POST",
+    headers : {
+        "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({key : value})
+})
+.then(res => res.json())
+.then( data => console.log(data))
+.catch(err => console.log(err))
+
+// DELETE method
+fetch(`${base_url} / ${id}`, {
+    method: "DELETE"
+})
+.then(res => res.json())
+.then(data => console.log("Product deleted successfuly"))
+.catch(error => console.log(error))
+
+//PATCH method
+fetch(`${base_url} / ${id}`, {
+    method: "PATCH",
+    header : {
+        "Content-Type" : "application/json"
+    },
+    body: JSON.stringify({key: value})
+})
 .then(res => res.json())
 .then(data => console.log(data))
-.catch(err => console.log(error))
+.catch(error => console.log(error))
 
-const greeting = first_name => first_name
+// CRUD=> 
+//     CREATE(post)
+//     READ(get)
+//     UPDATE(patch)
+//     DELETE
